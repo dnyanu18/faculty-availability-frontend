7927f4f2bb44a187204729a9eafe0310b76e873a
dnyanu18 <dnyaneshwarbpatil45@gmail.com>
2025-07-25 15:14:52 +0530

    Update TeacherDashboard.jsx

--- src/pages/TeacherDashboard.jsx
+++ src/pages/TeacherDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import API from "../services/api";
 import MeetingRequestsList from "../components/MeetingRequestsList";
 
